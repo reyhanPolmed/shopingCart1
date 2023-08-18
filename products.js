dfa44083fcafc619ef276e16f7584acf6135220a
reyhanPolmed <119090839+reyhanPolmed@users.noreply.github.com>
2023-08-18 16:16:50 +0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name : "iphone 12 pro",
+        desc : "6.1-inch display",
+        price: "566",
+        image: "https://res.cloudinary.com/dfcpydoq8/image/upload/v1689849391/samples/onlineShop/iphone12pro_bnidys.jpg"
+    },
+    {
+        id: 2,
+        name : "iphone 12",
+        desc : "5.4-inch display",
+        price: "345",
+        image: "https://res.cloudinary.com/dfcpydoq8/image/upload/v1689849399/samples/onlineShop/iphone12_n41era.jpg"
+    },
+    {
+        id: 3,
+        name : "iphone 10 pro",
+        desc : "6.1-inch display",
+        price: "232",
+        image: "https://res.cloudinary.com/dfcpydoq8/image/upload/v1689849356/samples/onlineShop/galaxyS_cbwyd7.png"
+    }
+]
+
+export default products;
